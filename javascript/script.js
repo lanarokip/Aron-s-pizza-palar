f111e3e5b1cd4d3cec63270e18f5acc1a317d390
lanarokip <lankipkoecharo@gmail.com>
2021-08-01 08:02:12 +0300

    added constrains holding the properties of pizza and added the variables we are going to use

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,7 @@
-function pPizza(type, size, crusty, toping , number, total ){
-    this.type = type;
+var price, cPrice, tPrice, totalPrice, Delivery, tTotal;
+function mainPizza(size, topping, crust, number){
     this.size = size;
-    this.crusty = crusty;
-    this.toping = toping;
+    this.topping = topping;
+    this.crust = crust;
     this.number = number;
-    this.total = total;
 }
-
-$(document).ready(function(){
-    $("#formPizza").submit(function(event){
-        var pType = $("#pizza").val();
-        var pSize = $("#pizzaSize").val();
-        var pCrusty = $("#crust").val();
-        var pToping = $("#topping").val();
-        var pnumber = $("#number").val();
-        var price = $("#list").append(pType)
-        var list2 =$('#list1').append(pSize)
-        event.preventDefault();
-        
-    })
- })
