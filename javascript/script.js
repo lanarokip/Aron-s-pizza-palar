d7a27320e946665320e67513f35419972dfd8294
lanarokip <lankipkoecharo@gmail.com>
2021-08-01 08:54:05 +0300

    added place holder for the total price

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,58 @@ function mainPizza(size, topping, crust, number){
     this.crust = crust;
     this.number = number;
 }
+$(document).ready(function(){
+    $("#formPizza").submit(function(event){
+        event.preventDefault();
+        var pizzaSize = $("#pizzaSize").val()
+        var crust = $("#crust").val()
+        var toppings = $("#topping").val()
+        var pizzaNumber = $("#number").val()
+        
+        switch(pizzaSize){
+            case "small":
+                price = 650;
+            break;
+            case "medium":
+                price = 750
+            break;
+            case "large":
+                price = 900;
+            break;
+            default:
+                price = 0;
+        }
+        
+        switch(crust){
+            case "thinCrust":
+                cPrice = 250;
+            break;
+            case "FlatbreadCrust":
+                cPrice = 300
+            break;
+            case "ThickCrust":
+                cPrice = 150;
+            break;
+            default:
+                cPrice = 200;
+        }
+        switch(topping){
+            case "tomato":
+                tPrice = 30;
+            break;
+            case "GreenPepper":
+                tPrice = 20
+            break;
+            case "mince":
+                tPrice = 10;
+            break;
+            default:
+                tPrice = 200;
+        }
+        Delivery = 250;
+        totalPrice = (tPrice + cPrice + price) * pizzaNumber
+        tTotal =( (tPrice + cPrice + price) * pizzaNumber)+Delivery
+        alert(tTotal)
+    })
+    
+})
